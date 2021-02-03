c8b753ff85ef67cc2fb39c9caec762fa4e4b27c2
NemusD <nemus@DESKTOP-N4D01U6.localdomain>
2021-02-03 16:30:42 -0300

    3er commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express ();
 
 app.listen(3000);
 
-app.use('/', express.static(__dirname + '/views'))
+app.use('/supermercado', express.static(__dirname + '/views'))
 
 console.log('Servidor funcionando')
